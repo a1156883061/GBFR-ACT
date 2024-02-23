9c4a3632a4a134c160549fefb59128138284ffef
罗皓元 <36263893+a1156883061@users.noreply.github.com>
2024-02-23 17:12:03 +0800

    feat: move to App.vue

--- gbfr-act-ui/src/main.ts
+++ gbfr-act-ui/src/main.ts
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus';
+import i18n from 'vue-i18n';
+import * as Icons from '@element-plus/icons';
+
+
+App.use(ElementPlus);
+App.use(i18n);
+createApp(App).mount('#app');
+
+Object.keys(Icons).forEach(key => {
+    App.component(key, Icons[key])
+})
